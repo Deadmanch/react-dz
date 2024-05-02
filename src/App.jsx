a3fbdf7f23fcc6108d158fe3d3c0e3988b305a07
Deadmanch <stepanova.nata.r@gmail.com>
2024-05-03 01:43:19 +0300

    Выполнено задание №1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css';
+import Button from './components/Button/Button';
+import Heading from './components/Heading/Heading';
+import Paragraph from './components/Paragraph/Paragraph';
 
 function App() {
-	return <div>First Commit</div>;
+	const data = [
+		{
+			title: 'Поиск',
+			text: 'Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.'
+		}
+	];
+	return (
+		<>
+			<Heading text={data[0].title} />
+			<Paragraph text={data[0].text} />
+			<Button text='Искать' />
+		</>
+	);
 }
 
 export default App;
