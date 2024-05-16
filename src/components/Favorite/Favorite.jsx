384404168e6d0960cd6509e9fc196dc4d7f06730
Deadmanch <stepanova.nata.r@gmail.com>
2024-05-16 16:47:25 +0300

    Исправил ошибку в добавлении стилей

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -4,7 +4,7 @@ import styles from './Favorite.module.css';
 const Favorite = ({ favorite }) => {
 	const label = favorite ? 'В избранном' : 'В избранное';
 	return (
-		<button className={cn(styles['favorite'], { [styles['favorite--active']]: favorite })}>
+		<button className={cn(styles['favorite'], { [styles['favorite_active']]: favorite })}>
 			<img src={!favorite ? '/icons/like.svg' : '/icons/bookmark.svg'} />
 			{label}
 		</button>
