b1dc68c489fbe8519d631436f4694a26ca2ce441
Deadmanch <stepanova.nata.r@gmail.com>
2024-05-29 12:57:24 +0300

    Добавил Forward Ref для Input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,29 @@
 import styles from './Input.module.css';
+import { forwardRef } from 'react';
 
-const Input = ({ icon, ...props }) => {
+const Input = forwardRef(({ icon, ...props }, ref) => {
 	return (
 		<div className={styles.wrapper}>
-			<input className={styles.input} {...props} type='text' placeholder='Введите название' />
-			{icon && <img className={styles.icon} src={icon} />}
+			<input
+				ref={ref}
+				className={styles.input}
+				{...props}
+				type='text'
+				placeholder={props.placeholder}
+			/>
+			{icon && (
+				<img
+					className={styles.icon}
+					src={icon}
+					width={24}
+					height={24}
+					alt='Иконка поиска'
+					loading='lazy'
+				/>
+			)}
 		</div>
 	);
-};
+});
+
+Input.displayName = 'Input';
 export default Input;
