3e5648df3edeb66f455126d568919d13ce47bedc
Deadmanch <stepanova.nata.r@gmail.com>
2024-05-29 12:58:55 +0300

    Добавил логику входа для Nav + добавил якорную ссылку временно для перемещения к форме входа

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,8 @@
+import { useContext } from 'react';
 import styles from './Nav.module.css';
+import { UserContext } from '../../context/user.context';
 const Nav = () => {
+	const { activeUser, logoutUser } = useContext(UserContext);
 	return (
 		<nav className={styles.nav}>
 			<ul className={styles.list}>
@@ -8,14 +11,41 @@ const Nav = () => {
 						Поиск фильмов
 					</a>
 				</li>
-				<li className={(styles.item, styles.active)}>
+				<li className={styles.item}>
 					<a className={styles.link} href='#'>
 						Мои фильмы
 					</a>
 				</li>
+				{activeUser && (
+					<li className={styles.item}>
+						<a className={styles.link} href='#'>
+							{activeUser.name}
+							<img
+								src='/icons/user.svg'
+								alt='Иконка пользователя'
+								loading='lazy'
+								width={24}
+								height={24}
+							/>
+						</a>
+					</li>
+				)}
 				<li className={styles.item}>
-					<a className={styles.link} href='#'>
-						Войти
+					<a className={styles.link} href='#login' onClick={activeUser && logoutUser}>
+						{activeUser?.isLogined ? (
+							'Выйти'
+						) : (
+							<>
+								Войти
+								<img
+									src='icons/login.svg'
+									alt='Иконка входа'
+									loading='lazy'
+									width={24}
+									height={24}
+								/>
+							</>
+						)}
 					</a>
 				</li>
 			</ul>
