d51358b768d2fb305f29924719e4004d6e92785c
Deadmanch <stepanova.nata.r@gmail.com>
2024-07-12 07:39:22 +0300

    Удалены хуки useAuth и useUser за ненадобностью

--- src/helpers/RequireAuthLoader.ts
+++ src/helpers/RequireAuthLoader.ts
@@ -1,11 +1,10 @@
 import { redirect } from 'react-router-dom';
 
-import { USERS_STORAGE_KEY } from '@/hooks/useAuth';
 import { IUser } from '@/interfaces/user.interface';
 
 const getActiveUser = async (): Promise<IUser | null> => {
-	const users: IUser[] = JSON.parse(localStorage.getItem(USERS_STORAGE_KEY) || '[]');
-	return users.find((user: IUser) => user.isLogged) || null;
+	const storageData = JSON.parse(localStorage.getItem('users') || '{}');
+	return storageData.activeUser || null;
 };
 
 export const RequireAuthLoader = async () => {
@@ -13,6 +12,5 @@ export const RequireAuthLoader = async () => {
 	if (!user) {
 		return redirect('/login');
 	}
-
 	return null;
 };
--- src/hooks/useUser.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from 'react';
-
-import { UserContext, UserContextType } from '../context/user.context';
-
-export const useUser = () => useContext<UserContextType>(UserContext);
