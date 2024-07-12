d7b3aadcb0dc66955bf102e3a110d9183cc2dc4f
Deadmanch <stepanova.nata.r@gmail.com>
2024-07-12 07:40:38 +0300

    Удален контекст из Layout

--- src/layouts/Layout/Layout.tsx
+++ src/layouts/Layout/Layout.tsx
@@ -1,23 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
-import UserContextProvider from '@/context/user.context';
-
 import Header from '../Header';
 
 import styles from './Layout.module.css';
 
-
-
-
 export const Layout = () => {
 	return (
-		<UserContextProvider>
-			<div className={styles.wrapper}>
-				<Header />
-				<main>
-					<Outlet />
-				</main>
-			</div>
-		</UserContextProvider>
+		<div className={styles.wrapper}>
+			<Header />
+			<main>
+				<Outlet />
+			</main>
+		</div>
 	);
 };
