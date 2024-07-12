77f28b787c13b59a4f7adb2c1e37ae2b149f93c9
Deadmanch <stepanova.nata.r@gmail.com>
2024-07-12 07:40:17 +0300

    Упростил interface фильмов и удалил movieByID за ненадобностью

--- src/interfaces/movie.interface.ts
+++ src/interfaces/movie.interface.ts
@@ -1,81 +1,28 @@
 export interface IMovie {
+	userId?: string;
 	id: number;
 	name: string;
-	alternativeName: string;
-	enName: string;
-	type: string;
-	year: number;
-	description: string;
-	shortDescription: string;
-	movieLength: number;
-	isSeries: boolean;
-	ticketsOnSale: boolean;
-	totalSeriesLength: unknown;
-	seriesLength: unknown;
-	ratingMpaa: string;
-	ageRating: number;
-	top10: unknown;
-	top250: number;
-	typeNumber: number;
-	status: unknown;
-	names: Name[];
-	externalId: ExternalId;
-	logo: Logo;
 	poster: Poster;
-	backdrop: Backdrop;
 	rating: Rating;
-	votes: Votes;
-	genres: Genre[];
-	countries: Country[];
-	releaseYears: unknown[];
-}
-
-export interface Name {
-	name: string;
-	language?: string;
+	description?: string;
 	type?: string;
-}
-
-export interface ExternalId {
-	imdb: string;
-	tmdb: number;
-	kpHD: string;
-}
-
-export interface Logo {
-	url: string;
-}
-
-export interface Poster {
-	url: string;
-	previewUrl: string;
-}
-
-export interface Backdrop {
-	url: string;
-	previewUrl: string;
-}
-
-export interface Rating {
-	kp: number;
-	imdb: number;
-	filmCritics: number;
-	russianFilmCritics: number;
-	await: unknown;
-}
-
-export interface Votes {
-	kp: number;
-	imdb: number;
-	filmCritics: number;
-	russianFilmCritics: number;
-	await: number;
+	year?: number;
+	movieLength?: number;
+	genres?: Genre[];
 }
 
 export interface Genre {
 	name: string;
 }
 
-export interface Country {
-	name: string;
+export interface Poster {
+	url: string;
+	previewUrl?: string;
+}
+
+export interface Rating {
+	kp?: number;
+	imdb?: number;
+	filmCritics?: number;
+	russianFilmCritics?: number;
 }
